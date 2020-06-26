01f4e9ebbfb0adc104271dd944ec7d5c66e0bca4
Manoj Kumar <mkumariitdelhi@gmail.com>
2020-06-26 17:21:57 +0530

    php file added

---
+++ contact.js
@@ -0,0 +1,40 @@
+function postToGoogle() {
+                var name = $("#name").val();
+                var email = $("#email").val();
+                val message = $("#message").val();
+        
+        if(name == ""){
+          alert('Please write your goodname.');
+          document.getElementById("name").focus();
+          return false;
+        }
+        if(email == ""){
+          alert('Plese write down your email address so that we can contact you if needed.');
+          document.getElementById("email").focus();
+          return false;
+        }
+        if(message == "")
+        {
+          alert('No message?? \n\n Please write down your message');
+          document.getElementById("message").focus();
+          return false;
+        }
+        
+                $.ajax({
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSfcP68DI5fjDGF49mPw1hdODF0WaK1vFfP0IA3XzkceA9TaOw/formResponse?",
+          data: {"entry.": name, "entry.": email, "entry.": message},
+
+
+             // data: {"entry.630525043": field1, "entry.707736545": field2, "entry.1786621969": field3, "entry.393785043": field4, "entry.393785043": height, "entry.1736541269":field7, "entry.1397015985":address},
+                    type: "POST",
+                    dataType: "xml",
+                    success: function(d)
+          {
+          },
+          error: function(x, y, z)
+            {
+                alert("Thanks for your message. \n\n May god bless you.");
+            }
+                });
+        return false;
+            }
---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+$errors = '';
+$mymail = 'mkumariitdelhi@gmail.com';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// if (!preg_match(
+// "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+// $email))
+// {
+//     $errors .= "\n Error: Invalid email address";
+// }
+
+if(empty($errors))
+{
+	$subject = "New Submission from your website : $name";
+	$body = "You have recieved a new message from your profile website. ".
+	" \nHere are the details:\n Name : $name \n Email: $email \n Message : \n $message";
+
+	$headers = "From : $mymail\n";
+	$headers .= "Reply-To : $email";
+
+	mail($mymail, $subject, $body, $headers);
+}
+?>
